e9eb2fcfb4484df293784ac9b6738018d5aa0e62
VelvetSyntax <imren_83@abv.bg>
2024-01-27 20:24:10 +0200

    getting elements by id

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-window.addEventListener('load', solve);
+window.addEventListener("load", solve);
 
 function solve() {
+  const timeInput = document.getElementById("time");
+  const dateInput = document.getElementById("date");
+  const placeInput = document.getElementById("place");
+  const eventInput = document.getElementById("event");
+  const emailInput = document.getElementById("email");
+  const addBtn = document.getElementById("add-btn");
+  const upcomingList = document.getElementById("upcoming-list");
+  const secondfield = document.getElementById("second-field");
+  const thirdfield = document.getElementById("third-field");
+  const clearBtn = document.getElementById("clear");
 
+  addBtn.addEventListener("click", onAdd);
+
+  function onAdd(event){
+    event.preventDefault();
+    
+  }
 }
-
-
-    
-    
